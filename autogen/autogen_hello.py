b424226422ea575a2c44820f0c719683c66d232c
Kumar Ashish <kumarashish2612@gmail.com>
2025-01-26 19:22:49 +0300

    autogen helloworld with groq api

---
+++ autogen/autogen_hello.py
@@ -0,0 +1,50 @@
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+import os
+import asyncio
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Define a tool
+async def get_weather(city: str) -> str:
+    return f"The weather in {city} is 73 degrees and Sunny."
+
+
+async def main() -> None:
+    # Define an agent
+    weather_agent = AssistantAgent(
+        name="weather_agent",
+        model_client=OpenAIChatCompletionClient(
+            model="llama-3.3-70b-versatile",
+            api_key=os.environ.get("GROQ_API_KEY"),
+            base_url="https://api.groq.com/openai/v1",
+            model_info={
+        "vision": True,
+        "function_calling": True,
+        "json_output": True,
+        "family": "unknown",
+    }
+        ),
+        tools=[get_weather],
+    )
+
+    # Define a team with a single agent and maximum auto-gen turns of 1.
+    agent_team = RoundRobinGroupChat([weather_agent], max_turns=1)
+
+    while True:
+        # Get user input from the console.
+        user_input = input("Enter a message (type 'exit' to leave): ")
+        if user_input.strip().lower() == "exit":
+            break
+        # Run the team and stream messages to the console.
+        stream = agent_team.run_stream(task=user_input)
+        await Console(stream)
+
+
+# NOTE: if running this inside a Python script you'll need to use asyncio.run(main()).
+#await main()
+asyncio.run(main())
